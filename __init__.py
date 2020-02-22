e1546724978e583b4f3faac5becaaf3f9dd15756
J. Nick Koston <nick@koston.org>
2020-02-22 09:43:31 -0600

    adj

--- __init__.py
+++ __init__.py
@@ -85,8 +85,8 @@ async def async_request_configuration(
         )
 
         if result == ValidationResult.INVALID_VERIFICATION_CODE:
-            await configurator.async_notify_errors(
-                _CONFIGURING[entry_id], "Invalid verification code"
+            configurator.async_notify_errors(
+                _CONFIGURING[entry_id], "Invalid verification code, please make sure you are using the latest code, and try again."
             )
         elif result == ValidationResult.VALIDATED:
             return await async_setup_august(
